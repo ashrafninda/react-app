1751ebd1df02ff70c89c0a384e73c83d8b4e4323
Ashraf Latif <ashraf.latif@nindatech.com>
2021-07-07 21:22:04 +0800

    create react app from scratch

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import React from "react";
+import { hot } from "react-hot-loader";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+export default hot(module)(App);
